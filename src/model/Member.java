7e635a243362509d302bbbeb3cae64780048b8b6
v1b3s <146864395+v1b3s0@users.noreply.github.com>
2026-06-10 22:14:47 +0400

    Adjusted README and added basic Member.java

---
+++ src/model/Member.java
@@ -0,0 +1,56 @@
+package model;
+
+public class Member {
+    private String name;
+    private int age;
+    private String phoneNumber;
+    private String email;
+    private String gender;
+    private String membershipType;
+    private String fitnessGoals;
+    private String notes;
+
+    public Member(String name, int age, String phoneNumber, String email,
+            String gender, String membershipType, String fitnessGoals, String notes) {
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.gender = gender;
+        this.membershipType = membershipType;
+        this.fitnessGoals = fitnessGoals;
+        this.notes = notes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getMembershipType() {
+        return membershipType;
+    }
+
+    public String getFitnessGoals() {
+        return fitnessGoals;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+}
